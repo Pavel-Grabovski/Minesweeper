4f7b2a798f5f9087aa75287a29c8be9613acf888
PavelGrabovskiy <Araa9977@mail.ru>
2025-02-12 19:59:03 +0700

    Подключена библиотека TG и создан клиент TG

---
+++ MinesweeperConsoleApp/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace MinesweeperConsoleApp;
+
+public static class Constants
+{
+    public const string Key = "8051446835:AAG4QSXStU5pkQ0c2VZM85JILBLXahUstHY";
+}
--- MinesweeperConsoleApp/Program.cs
+++ MinesweeperConsoleApp/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace MinesweeperConsoleApp
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace MinesweeperConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Start MinesweeperConsoleApp!");
+        TelegramBotClient tgClient = new(Constants.Key);
+        tgClient.StartReceiving(HandleUpdate, HandleError);
+        Console.WriteLine("Bot TG started!");
+        Console.Read();
+    }
+
+    private static async Task HandleError(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+        throw new NotImplementedException();
     }
 }
