9fca0b51855f2e382cbb4a92b9e6abdd53721c92
PavelGrabovskiy <Araa9977@mail.ru>
2025-02-13 18:52:12 +0700

    перенес логику в  TGChatBotService

--- MinesweeperConsoleApp/Program.cs
+++ MinesweeperConsoleApp/Program.cs
@@ -1,55 +1,14 @@
-﻿using Telegram.Bot;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
-
-namespace MinesweeperConsoleApp;
+﻿namespace MinesweeperConsoleApp;
 
 internal class Program
 {
-    private static TelegramBotClient tgClient;
     static void Main(string[] args)
     {
-        Console.WriteLine("Start MinesweeperConsoleApp!");
-        tgClient = new(Constants.Token);
-        tgClient.StartReceiving(HandleUpdate, HandleError);
+        Console.WriteLine("Start MinesweeperConsoleApp.Main");
+
+        TGChatBotService _tgClient = new(Constants.Token); 
+
         Console.WriteLine("Bot TG started!");
         Console.Read();
-    }
-
-
-
-    private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
-    {
-
-        if(update.Type == UpdateType.Message)
-        {
-            Message? message = update.Message;
-
-            if (message?.Text == "/start")
-                await CreateStartButtons(message);
-        }
-        else if(update.Type == UpdateType.CallbackQuery)
-        {
-            Message? message = update.CallbackQuery?.Message;
-
-        }
-    }
-
-    private static async Task CreateStartButtons(Message message)
-    {
-        var keyboard = new InlineKeyboardMarkup([
-                [InlineKeyboardButton.WithCallbackData("Начать играть", "/start_game")],
-                [InlineKeyboardButton.WithCallbackData("Правила", "/rules")],
-                [InlineKeyboardButton.WithCallbackData("Статистика", "/statistics")]
-            ]);
-
-        await tgClient.SendMessage(message.Chat.Id, "Выберете действие:", replyMarkup: keyboard);
-    }
-
-    private static async Task HandleError(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
-    {
-        Console.WriteLine(exception.Message);
     }
 }
---
+++ MinesweeperConsoleApp/TGChatBotService.cs
@@ -0,0 +1,50 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace MinesweeperConsoleApp;
+
+public class TGChatBotService
+{
+    private readonly TelegramBotClient _tgClient;
+    public TGChatBotService(string token)
+    {
+        _tgClient = new(token);
+        _tgClient.StartReceiving(HandleUpdate, HandleError);
+    }
+
+    private async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
+    {
+
+        if (update.Type == UpdateType.Message)
+        {
+            Message? message = update.Message;
+
+            if (message?.Text == "/start")
+                await CreateStartButtons(message);
+        }
+        else if (update.Type == UpdateType.CallbackQuery)
+        {
+            Message? message = update.CallbackQuery?.Message;
+
+        }
+    }
+
+    private async Task CreateStartButtons(Message message)
+    {
+        var keyboard = new InlineKeyboardMarkup([
+                [InlineKeyboardButton.WithCallbackData("Начать играть", "/start_game")],
+                [InlineKeyboardButton.WithCallbackData("Правила", "/rules")],
+                [InlineKeyboardButton.WithCallbackData("Статистика", "/statistics")]
+            ]);
+
+        await _tgClient.SendMessage(message.Chat.Id, "Выберете действие:", replyMarkup: keyboard);
+    }
+
+    private async Task HandleError(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+    {
+        Console.WriteLine(exception.Message);
+    }
+}
